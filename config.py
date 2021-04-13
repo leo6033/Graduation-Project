5a294a10048a08ec2136bfa892c2b5dde0734dda
leo6033 <long452a@163.com>
2021-04-13 22:41:45 +0800

    Update config file

--- config.py
+++ config.py
@@ -9,5 +9,13 @@ class Config:
     gender_embedding_output_dim = 16
     age_embedding_output_dim = 16
     occupation_embedding_output_dim = 16
+    move_id_embedding_output_dim = 32
+    movie_categories_output_num = 32
 
     embedding_dim = 32
+
+    movie_categories_num = 18
+    move_id_num = 3953
+
+    title_max_len = 16
+    generes_max_len = 6
